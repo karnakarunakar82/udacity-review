ada2affe995effc000bc6882dae03c7b419dfa73
karnakarunakar82 <48981157+karnakarunakar82@users.noreply.github.com>
2019-06-25 16:31:14 +0530

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.alt=restaurant.name;
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = "./"+DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
